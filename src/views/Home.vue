34e84c792ee2213ce77b1338be8df288efd4b085
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-27 16:10:47 -0300

    Criando homepage simples

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div class="position_center">
+        <h2 class="title is-2">Bem vindo a pagina inicial do projeto!</h2>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.position_center {
+    text-align: center;
+}
+</style>
