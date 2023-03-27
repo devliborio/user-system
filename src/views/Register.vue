f69cbc5c54923cc6a8214b4a0872b0d157078945
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-27 16:22:19 -0300

    Criando sistema de registro de usuário

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,33 +3,46 @@
     <div class="position_center">
         <h2 class="title is-4 mt-5">Cadastro de Usuário</h2>
 
+        <div v-if="error != undefined">
+            <div class="notification is-danger is-light">
+                {{ error }} <br>
+            </div>
+        </div>
+
+        <div v-if="success != undefined">
+            <div class="notification is-primary is-light">
+                {{ success }} <br>
+            </div>
+        </div>
+
+        <br>
         <div class="field">
             <p class="control">
-                <input class="input input_large" type="text" placeholder="Digite o nome do usuário">
+                <input class="input input_large" type="text" placeholder="nome" v-model="name">
             </p>
         </div>
 
         <div class="field">
             <p class="control">
-                <input class="input input_large" type="email" placeholder="email@email.com">
+                <input class="input input_large" type="email" placeholder="email@email.com" v-model="email">
             </p>
         </div>
 
         <div class="field">
             <p class="control">
-                <input class="input input_large" type="password" placeholder="*********">
+                <input class="input input_large" type="password" placeholder="*********" v-model="password">
             </p>
         </div>
 
         <div class="field">
             <p class="control">
-                <input class="input input_large" type="number" placeholder="Escolha o cargo 0 ou 1">
+                <input class="input input_large" type="number" placeholder="Escolha o cargo 0 ou 1" v-model="role">
             </p>
         </div>
 
         <div class="field">
             <p class="control">
-                <button class="button is-success btn_large">Cadastrar</button>
+                <button @click="register()" class="button is-success btn_large">Cadastrar</button>
             </p>
         </div>
 
@@ -37,8 +50,35 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+    data() {
+        return {
+            name: '',
+            password: '',
+            email: '',
+            role: 0,
+            error: undefined,
+            success: undefined
+        }
+    },
 
+    methods: {
+        register() {
+            axios.post("http://localhost:8686/user", {
+                name: this.name,
+                password: this.password,
+                email: this.email,
+                role: this.role,
+            }).then((res) => { 
+                console.log(res) 
+                this.success = "Usuário cadastrado com sucesso!"
+            }).catch((err) => { 
+                let msgErro = err.response.data.msg
+                this.error = msgErro;
+            });
+        }
+    }
 }
 </script>
 
@@ -47,7 +87,8 @@ export default {
 .btn_large {
     width: 50%;
 }
-.position_center{
+
+.position_center {
     text-align: center;
 }
 </style>
