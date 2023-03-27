4c330eb583e1c9e57e1201e74cd3814898fbabe3
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-27 11:55:57 -0300

    Criando HTML do formulário de cadastro com bulma

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,16 +1,53 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div>
-        <h1>Registro de Usuário</h1>
+    <div class="position_center">
+        <h2 class="title is-4 mt-5">Cadastro de Usuário</h2>
+
+        <div class="field">
+            <p class="control">
+                <input class="input input_large" type="text" placeholder="Digite o nome do usuário">
+            </p>
+        </div>
+
+        <div class="field">
+            <p class="control">
+                <input class="input input_large" type="email" placeholder="email@email.com">
+            </p>
+        </div>
+
+        <div class="field">
+            <p class="control">
+                <input class="input input_large" type="password" placeholder="*********">
+            </p>
+        </div>
+
+        <div class="field">
+            <p class="control">
+                <input class="input input_large" type="number" placeholder="Escolha o cargo 0 ou 1">
+            </p>
+        </div>
+
+        <div class="field">
+            <p class="control">
+                <button class="button is-success btn_large">Cadastrar</button>
+            </p>
+        </div>
+
     </div>
 </template>
 
 <script>
 export default {
-    
+
 }
 </script>
 
 <style scoped>
-
+.input_large,
+.btn_large {
+    width: 50%;
+}
+.position_center{
+    text-align: center;
+}
 </style>
