47c278c08196f3e99181bdbcae5c7e66e8022ba9
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-28 14:00:47 -0300

    Criando navbar com Bulma

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,51 @@
 <template>
   <div id="app">
-    <nav>
-      <a><router-link to="/">Home</router-link> | </a>
-      <a><router-link to="/register">Cadastrar-se</router-link> | </a>
-      <a><router-link to="/login">Login</router-link></a>
-    </nav>
+
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+
+         <div class="navbar-brand">
+            <a class="navbar-item" href="/"><img src="./assets/logo_v1.png" width="" height="500"></a>
+
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item"><router-link to="/">Home</router-link></a>
+      <a class="navbar-item"><router-link to="/register">Registro de Usuário</router-link></a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">More</a>
+
+        <div class="navbar-dropdown">
+          <!-- <a class="navbar-item">About</a>
+          <a class="navbar-item">Jobs</a>
+          <a class="navbar-item">Contact</a>
+          <hr class="navbar-divider">
+          <a class="navbar-item">Report an issue</a> -->
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="navbar-end">
+
+      <div class="navbar-item">
+        <div class="buttons">
+          <!-- <a class="button is-primary"><strong>Sign up</strong></a> -->
+          <button class="button is-light"><router-link to="/login">Login</router-link></button>
+        </div>
+      </div>
+
+    </div>
+    
+  </div>
+</nav>
     <router-view />
   </div>
 </template>
