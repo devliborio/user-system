658b028b5063710d9635e69f494fc1a11bf43c0b
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-29 18:32:39 -0300

    Criando router guard para validação de token

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,37 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Register from '../views/Register.vue'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import axios from 'axios'
 
 const routes = [
   {
     path: '/register',
     name: 'register',
-    component: Register
+    component: Register,
+    beforeEnter: (to, from, next) => {
+      if (localStorage.getItem('token') != undefined) {
+
+        var request = {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem('token')
+          }
+        }
+
+        console.log(request)
+
+        axios.post("http://localhost:8686/validate",{},request).then((res) => {
+          console.log(res)
+          next();
+        }).catch((err) => {
+          let errorAuth = err.response.data;
+          alert(errorAuth);
+          next('/login');
+        });
+      } else {
+        alert("Faça o login primeiro!");
+        next('/login');
+      }
+    }
   },
 
   {
