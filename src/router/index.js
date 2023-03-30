12ab25214a06793ad23cbf9cf4e68fb0168dd820
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-30 13:33:40 -0300

    Encapsulando função de validação

--- src/router/index.js
+++ src/router/index.js
@@ -4,33 +4,35 @@ import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import axios from 'axios'
 
+function AdminAuth(to, from, next) {
+  if (localStorage.getItem('token') != undefined) {
+
+    var request = {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem('token')
+      }
+    }
+
+    axios.post("http://localhost:8686/validate", {}, request).then((res) => {
+      console.log(res)
+      next();
+    }).catch((err) => {
+      let errorAuth = err.response.data;
+      alert(errorAuth);
+      next('/login');
+    });
+  } else {
+    alert("Faça o login primeiro!");
+    next('/login');
+  }
+}
+
 const routes = [
   {
     path: '/register',
     name: 'register',
     component: Register,
-    beforeEnter: (to, from, next) => {
-      if (localStorage.getItem('token') != undefined) {
-
-        var request = {
-          headers: {
-            Authorization: "Bearer " + localStorage.getItem('token')
-          }
-        }
-
-        axios.post("http://localhost:8686/validate",{},request).then((res) => {
-          console.log(res)
-          next();
-        }).catch((err) => {
-          let errorAuth = err.response.data;
-          alert(errorAuth);
-          next('/login');
-        });
-      } else {
-        alert("Faça o login primeiro!");
-        next('/login');
-      }
-    }
+    beforeEnter: AdminAuth
   },
 
   {
